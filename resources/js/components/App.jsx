d92aa24f3d22a05d93efdf36f18807b1dec3b146
arashsamandar <arash.internet@gmail.com>
2024-09-02 00:59:01 +0330

    doneing

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function App(){
     return(
-        <header className="bg-blue-500 full-width text-center">
-            Welcome Message Fron ReactJs
-        </header>
+        <div className="container">
+            <h1>Hello World</h1>
+        </div>
     )
 }
